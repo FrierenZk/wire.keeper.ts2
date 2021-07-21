05fd6f164841ea6a4910bccfde80e9021323e954
FrierenZk <lingfengfroze@yahoo.com>
2021-07-21 10:44:14 +0800

    Add sidebar

--- src/index-renderer.ts
+++ src/index-renderer.ts
@@ -1,3 +1,5 @@
+import {Sidebar} from "./ui/sidebar";
+
 document.getElementById('tail_script')?.remove()
 
 let shadow = document.createElement('div')
@@ -5,5 +7,10 @@ shadow.className = 'w-100 border-bottom top-shadow'
 document.body.appendChild(shadow)
 
 let main = document.createElement('main')
+main.className = 'flex-row'
 document.body.appendChild(main)
 
+let sideBar = document.createElement('div')
+sideBar.className = 'side-bar mh-100 bg-white flex-column'
+main.appendChild(sideBar)
+sideBar.appendChild(new Sidebar().createFragment())
