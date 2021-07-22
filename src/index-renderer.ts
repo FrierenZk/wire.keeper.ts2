dddc9142d309707e543c103ac8258d9c48d8b3bf
FrierenZk <lingfengfroze@yahoo.com>
2021-07-22 13:07:21 +0800

    Add modal & toast support

--- src/index-renderer.ts
+++ src/index-renderer.ts
@@ -1,5 +1,7 @@
 import {Sidebar} from "./ui/sidebar";
 import {Tooltip} from "bootstrap";
+import {Modals} from "./ui/modals";
+import {Toasts} from "./ui/toasts";
 
 document.getElementById('tail_script')?.remove()
 
@@ -16,7 +18,31 @@ sideBar.className = 'side-bar mh-100 bg-white flex-column'
 main.appendChild(sideBar)
 sideBar.appendChild(Sidebar.createFragment())
 
+let modals = new Modals()
+modals.init(document.body)
+
+let toasts = new Toasts()
+toasts.init(document.body)
+
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 tooltipTriggerList.map((tooltipTriggerEl) => {
     return new Tooltip(tooltipTriggerEl)
 });
+
+function showAddModal() {
+    modals.showAddModal()
+}
+
+function showConfirmModal(text: string, callback: () => void) {
+    modals.showConfirmModal(text, callback)
+}
+
+function showAlert(msg: string) {
+    toasts.showAlert(msg)
+}
+
+function showToast(msg: string) {
+    toasts.showToast(msg)
+}
+
+export {showAddModal, showConfirmModal, showAlert, showToast}
