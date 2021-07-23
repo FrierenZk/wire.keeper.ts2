033d03e74c12f5136c8d8dc57e3cd0861b9d502f
FrierenZk <lingfengfroze@yahoo.com>
2021-07-23 08:48:47 +0800

    Use stylesheet instead of bootstrap class

--- src/index-renderer.ts
+++ src/index-renderer.ts
@@ -6,15 +6,14 @@ import {Toasts} from "./ui/toasts";
 document.getElementById('tail_script')?.remove()
 
 let shadow = document.createElement('div')
-shadow.className = 'w-100 border-bottom top-shadow'
+shadow.className = 'top-shadow'
 document.body.appendChild(shadow)
 
 let main = document.createElement('main')
-main.className = 'flex-row'
 document.body.appendChild(main)
 
 let sideBar = document.createElement('div')
-sideBar.className = 'side-bar mh-100 bg-white flex-column'
+sideBar.className = 'side-bar'
 main.appendChild(sideBar)
 sideBar.appendChild(Sidebar.createFragment())
 
