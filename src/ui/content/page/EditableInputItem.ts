40751a69b16f4e203078d43b12cc4fa9b01810f3
FrierenZk <lingfengfroze@yahoo.com>
2021-12-17 14:24:27 +0800

    add: add editable input item

---
+++ src/ui/content/page/EditableInputItem.ts
@@ -0,0 +1,65 @@
+import {ObserverInputField} from "../../databingding/ObserverInputField";
+
+class EditableInputItem {
+    create(title: string, observer: ObserverInputField<any>) {
+        let fragment = document.createDocumentFragment()
+
+        let inputGroup = document.createElement('div')
+        fragment.appendChild(inputGroup)
+        inputGroup.className = 'input-group'
+        inputGroup.innerHTML = `<span class="input-group-text user-select-none fw-bold">${title}</span>`
+
+        let input = document.createElement('input')
+        inputGroup.appendChild(input)
+        input.className = 'form-control'
+        input.setAttribute('disabled', '')
+        input.setAttribute('readonly', '')
+        observer.setBinding(input)
+
+        let editBtn = document.createElement('button')
+        inputGroup.appendChild(editBtn)
+        editBtn.type = 'button'
+        editBtn.className = 'btn btn-outline-primary px-2'
+        editBtn.innerHTML = `<i class="bi bi-text-indent-left"></i>`
+
+        let checkBtn = document.createElement('button')
+        checkBtn.type = 'button'
+        checkBtn.className = 'btn btn-outline-primary px-2'
+        checkBtn.innerHTML = `<i class="bi bi-check"></i>`
+
+        let xBtn = document.createElement('button')
+        xBtn.type = 'button'
+        xBtn.className = 'btn btn-outline-danger px-2'
+        xBtn.innerHTML = `<i class="bi bi-x"></i>`
+
+        let tmp = observer.get()
+        editBtn.addEventListener('click', ev => {
+            ev.cancelBubble
+            tmp = observer.get()
+            inputGroup.removeChild(editBtn)
+            inputGroup.appendChild(checkBtn)
+            inputGroup.appendChild(xBtn)
+            input.removeAttribute('disabled')
+            input.removeAttribute('readonly')
+        })
+
+        checkBtn.addEventListener('click', ev => {
+            ev.cancelBubble
+            inputGroup.removeChild(checkBtn)
+            inputGroup.removeChild(xBtn)
+            inputGroup.appendChild(editBtn)
+            input.setAttribute('disabled', '')
+            input.setAttribute('readonly', '')
+        })
+
+        xBtn.addEventListener('click', ev => {
+            ev.cancelBubble
+            observer.set(tmp)
+            checkBtn.click()
+        })
+
+        return fragment
+    }
+}
+
+export {EditableInputItem}
